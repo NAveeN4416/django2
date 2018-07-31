535f06469a64e87caef269e039c3f79e6cb24583
naveen4416 <39522059+naveen4416@users.noreply.github.com>
2018-08-01 00:16:45 +0530

    Update redirections.py

--- django2/core/redirections.py
+++ django2/core/redirections.py
@@ -1,1 +1,11 @@
-qwer
+
+# Redirections
+
+def redirection(request):
+    if request.user.is_authenticated:
+        if request.user.is_staff | request.user.is_superuser:
+            return True
+        else:
+            return False
+    else:
+        return False
