40b5ed61ef492e95d79b3757d2e8abcc99b01713
naveen4416 <39522059+naveen4416@users.noreply.github.com>
2018-08-01 14:55:42 +0530

    Update views.py

--- django2/home/views.py
+++ django2/home/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# import Requirements Here
+
+from django.shortcuts import render,redirect,reverse
+from django.conf import settings
+import os
+import os.path import join
+import datetime
+
+
+from django.contrib.auth.models import User
+#from .models import 
+from django.http import HttResponse ,HttpRequest, JsonResponse
+
+
+
+#=============================================
 
 # Create your views here.
+
+# Declare Models as Constants
+constants = {
+              'User' : User
+              }
