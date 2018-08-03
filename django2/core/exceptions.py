ff11855931ff2e8db22fc4217f595297b651008a
naveen4416 <39522059+naveen4416@users.noreply.github.com>
2018-08-03 17:55:36 +0530

    Create exceptions.py

---
+++ django2/core/exceptions.py
@@ -0,0 +1,16 @@
+'''
+
+Extend Every New Class with APIException
+e.g : class newclass(APIException):
+  def newdef():
+    pass
+
+'''
+
+# Import Requirements Here
+
+from rest_framework import exceptions
+
+
+# Write Your Exceptional Classes here
+
